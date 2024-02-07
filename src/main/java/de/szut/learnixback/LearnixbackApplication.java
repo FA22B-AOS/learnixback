50d186c40795445797cbe6b2532c671ab3cae812
alpha.dango98@gmail.com <AlphaDango>
2024-02-07 10:33:18 +0100

    first commit

---
+++ src/main/java/de/szut/learnixback/LearnixbackApplication.java
@@ -0,0 +1,13 @@
+package de.szut.learnixback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnixbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnixbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/szut/learnixback/LearnixbackApplicationTests.java
@@ -0,0 +1,13 @@
+package de.szut.learnixback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnixbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
