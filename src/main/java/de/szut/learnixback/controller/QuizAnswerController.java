599a6edd629b2aba4ce75a017b326535040607d4
alpha.dango98@gmail.com <AlphaDango>
2024-05-28 13:58:39 +0200

    LEARNIX-9 Arbeitsstand update

--- src/main/java/de/szut/learnixback/controller/QuizAnswerController.java
+++ src/main/java/de/szut/learnixback/controller/QuizAnswerController.java
@@ -1,6 +1,7 @@
 package de.szut.learnixback.controller;
 
 import de.szut.learnixback.entities.QuizAnswer;
+import de.szut.learnixback.services.QuizAnswerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,14 +16,14 @@ public class QuizAnswerController {
     private QuizAnswerService quizAnswerService;
 
     @PostMapping
-    public ResponseEntity<QuizAnswer> submitAnswer(@RequestParam Long quizId, @RequestParam int selectedOption, @RequestParam UUID userId) {
+    public ResponseEntity<QuizAnswer> submitAnswer(@RequestParam Long quizId, @RequestParam int selectedOption, @RequestParam String userId) {
         QuizAnswer savedAnswer = quizAnswerService.saveAnswer(quizId, selectedOption, userId);
         return new ResponseEntity<>(savedAnswer, HttpStatus.CREATED);
     }
 
     @GetMapping("/score")
-    public int getTotalScore(@RequestParam UUID userId) {
-        return quizAnswerService.getTotalScore(userId);
+    public int getTotalScore(@RequestParam String userId) {
+        return quizAnswerService.getTotalScore(userId,0L);
     }
 }
 
