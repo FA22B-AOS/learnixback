22aa3eaa7114291d472ee1b52f8692d0f423650c
Alex <Alex>
2024-05-29 12:37:19 +0200

    LEARNIX-90 Renamed WorkspaceNotFoundException to RessourceNotFoundException

--- src/main/java/de/szut/learnixback/customExceptionHandling/RessourceNotFoundException.java
+++ src/main/java/de/szut/learnixback/customExceptionHandling/RessourceNotFoundException.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class WorkspaceNotFoundException extends RuntimeException {
-    public WorkspaceNotFoundException(String message) {
+public class RessourceNotFoundException extends RuntimeException {
+    public RessourceNotFoundException(String message) {
         super(message);
     }
 }
